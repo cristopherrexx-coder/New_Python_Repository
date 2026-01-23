5f0d3d69a79b7271982d858ebd9ed75a58d59cc1
Cristopher <cristopher.rexx@gmail.com>
2026-01-23 11:48:39 -0600

    First Expantion

--- app.py
+++ app.py
@@ -2,4 +2,10 @@ import streamlit as st
 
 st.header('Lanzar una moneda')
 
+number_of_tirals = st.slider("¿Número de intenos?", 1, 1000, 10)
+start_button = st.button("Ejecutar")
+
+if start_button:
+    st.write(f'Experimento con {number_of_tirals} intentos en curso.')
+
 st.write('Esta aplicación aún no es funcional. En construcción.')
